b431a13c439cc96c4377491a28d88930d7ea72da
marrs21 <ashledep@oregonstate.edu>
2024-08-05 19:18:24 -0700

    File Organization
    Seperate folders from past work and progress from today. Switched to remote host in order to use node and others and work live from site

---
+++ Work from Remote Host/GroupProject/app.js
@@ -0,0 +1,72 @@
+/*
+    SETUP
+*/
+var express = require('express');   // We are using the express library for the web server
+var app     = express();            // We need to instantiate an express object to interact with the server in our code
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+PORT        = 3697;                 // Set a port number at the top so it's easy to change in the future
+var db = require('./database/db-connector') //database
+
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+
+
+/*
+    ROUTES
+*/
+app.post('/add-coach-ajax', function(req, res) 
+{
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Capture NULL values
+    let name = parseInt(data.name);
+    if (isNaN(name))
+    {
+        name = 'NULL'
+    }
+
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Coaches (name, email, trainingComplete) VALUES ('${data.name}', '${data.email}', ${trainingComplete})`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+        else
+        {
+            // If there was no error, perform a SELECT * on Coaches
+            query2 = `SELECT * FROM Coaches;`;
+            db.pool.query(query2, function(error, rows, fields){
+
+                // If there was an error on the second query, send a 400
+                if (error) {
+                    
+                    // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                    console.log(error);
+                    res.sendStatus(400);
+                }
+                // If all went well, send the results of the query back.
+                else
+                {
+                    res.send(rows);
+                }
+            })
+        }
+    })
+});
+/*
+    LISTENER
+*/
+app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+});
---
+++ Work from Remote Host/GroupProject/public/js/add_coach.js
@@ -0,0 +1,75 @@
+let addCoachForm = document.getElementById('add-coach-form-ajax');
+
+addCoachForm.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    let inputName = document.getElementById("input-name");
+    let inputEmail = document.getElementById("input-email");
+    let inputTraining = document.getElementById("input-training");
+
+    let nameValue = inputname.value;
+    let emailValue = inputemail.value;
+    let trainingValue = inputtraining.value;
+
+    let data = {
+        name: nameValue,
+        email: emailValue,
+        trainingComplete: trainingValue
+    }
+
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/add-coach-ajax", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+            // Add the new data to the table
+            addRowToTable(xhttp.response);
+
+            // Clear the input fields for another transaction
+            inputName.value = '';
+            inputEmail.value = '';
+            inputTraining.value = '';
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            console.log("There was an error with the input.")
+        }
+    }
+
+    xhttp.send(JSON.stringify(data));
+
+})
+addRowToTable = (data) => {
+
+    // Get a reference to the current table on the page and clear it out.
+    let currentTable = document.getElementById("coach-table");
+
+    // Get the location where we should insert the new row (end of table)
+    let newRowIndex = currentTable.rows.length;
+
+    // Get a reference to the new row from the database query (last object)
+    let parsedData = JSON.parse(data);
+    let newRow = parsedData[parsedData.length - 1]
+
+    let row = document.createElement("TR");
+    let idCell = document.createElement("TD");
+    let nameCell = document.createElement("TD");
+    let emailCell = document.createElement("TD");
+    let trainingCell = document.createElement("TD");
+
+    idCell.innerText = newRow.id;
+    nameCell.innerText = newRow.name;
+    emailCell.innerText = newRow.email;
+    trainingCell.innerText = newRow.training;
+
+    row.appendChild(idCell);
+    row.appendChild(nameCell);
+    row.appendChild(emailCell);
+    row.appendChild(trainingCell);
+    
+    // Add the row to the table
+    currentTable.appendChild(row);
+}
